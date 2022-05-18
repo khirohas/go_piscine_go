175a00e1c6f4c23cb2ac2094870266e03101cd4f
khirohas <keihiro84@gmail.com>
2022-05-18 10:42:46 +0900

    finished ex05

--- ex04/vendor/piscine/printcomb.go
+++ ex04/vendor/piscine/printcomb.go
@@ -3,5 +3,25 @@ package piscine
 import "ft"
 
 func PrintComb() {
-	
+
+	f, s, t := 0, 0, 0
+	for f = 0; f <= 9; f++ {
+		for s = 0; s <= 9; s++ {
+			for t = 0; t <= 9; t++ {
+				if f < s && s < t {
+					outputComb(f, s, t)
+				}
+			}
+		}
+	}
+}
+
+func outputComb(f, s, t int) {
+	ft.PrintRune((rune)('0' + f))
+	ft.PrintRune((rune)('0' + s))
+	ft.PrintRune((rune)('0' + t))
+	if !(f == 7 && s == 8 && t == 9) {
+		ft.PrintRune(',')
+		ft.PrintRune(' ')
+	}
 }
---
+++ ex05/vendor/piscine/printcomb2.go
@@ -0,0 +1,53 @@
+package piscine
+
+import "ft"
+
+func PrintComb2() {
+
+	f := [2]int{0, 0}
+	s := [2]int{0, 0}
+	/*
+	   	for ; f[0] <= 9; f[0]++ {
+	   		for ; f[1] <= 9; f[1]++ {
+	   			for ; s[0] <= 9; s[0]++ {
+	   				for ; s[1] <= 9; s[1]++ {
+	   					if f[0] != s[0] && f[1] != f[0] && s[0] != s[1] {
+	   						outputComb(f, s)
+	   					}
+	   				}
+	   			}
+	   		}
+	   	}
+	   }
+	*/
+	for f[0] = 0; f[0] <= 9; f[0]++ {
+		for f[1] = 0; f[1] <= 9; f[1]++ {
+			for s[0] = 0; s[0] <= 9; s[0]++ {
+				for s[1] = 0; s[1] <= 9; s[1]++ {
+					if f != s && isMorethan(s, f) {
+						outputComb(f, s)
+					}
+				}
+			}
+		}
+	}
+}
+
+func isMorethan(s, f [2]int) bool {
+
+	if s[0] > f[0] || (s[0] == f[0] && s[1] > f[1]) {
+		return (true)
+	} else {
+		return (false)
+	}
+}
+
+func outputComb(f, s [2]int) {
+	ft.PrintRune((rune)('0' + f[0]))
+	ft.PrintRune((rune)('0' + f[1]))
+	ft.PrintRune(' ')
+	ft.PrintRune((rune)('0' + s[0]))
+	ft.PrintRune((rune)('0' + s[1]))
+	ft.PrintRune(',')
+	ft.PrintRune('\n')
+}
